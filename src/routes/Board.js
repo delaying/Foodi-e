14a8c494a1f429cb13ed042f9e9a33acf2261231
delaying <pgy5638@naver.com>
2022-04-14 21:58:06 +0900

    file 수정, css 수정

--- src/routes/Board.js
+++ src/routes/Board.js
@@ -1,11 +1,88 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import freeIcon from "../img/freeIcon.svg";
+import changeIcon from "../img/changeIcon.svg";
+import partyIcon from "../img/partyIcon.svg";
+import recipeIcon from "../img/recipeIcon.svg";
+import {RiArrowDropDownLine} from "react-icons/ri";
+import {HiSearch} from "react-icons/hi";
+import mainIcon from "../img/mainimage.svg";
+import {Link} from "react-router-dom";
+import {TiPencil} from "react-icons/ti";
+import {useLocation} from "react-router-dom";
+import {FaRegHeart} from "react-icons/fa";
+import {useParams} from 'react-router-dom';
+import "./board.css";
 import Post from "../component/PostComponent";
 
-function Board(){
-    return(
 
-        <Post/>
+function Board() {
+    // const [postname, setPostname] = useState('나눔');
+    // const {data} = useParams();
+    let name = '나눔';
+    let icon = freeIcon;
+    const data = useLocation().pathname;
+
+    if (data == '/post/free') {
+        name = '나눔';
+        icon = freeIcon;
+    } else if (data == '/post/change') {
+        name = '교환';
+        icon = changeIcon;
+
+    } else if (data == '/post/party') {
+        name = '푸대원';
+        icon = partyIcon;
+
+    } else {
+        name = '레시피';
+        icon = recipeIcon;
+    }
+
+
+    return (
+        <div className="postpage">
+            <div className="post-header">
+                <Link to="/"><img src={icon} alt=""/></Link>
+                <h4>{name} 게시판</h4>
+                <span className="header-drop-icon">
+                    <RiArrowDropDownLine size="35"/>
+                </span>
+                <span className="header-search-icon">
+                    <HiSearch size={20}/>
+                </span>
+                <span className="header-main-img">
+                    <img src={mainIcon} alt=""/>
+                </span>
+            </div>
+            <div className="choice-area">
+                <span className="area-text">덕명동</span>
+                <span className="area-dropdown-icon">
+                    <RiArrowDropDownLine size="25"/>
+                </span>
+            </div>
+
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+
+
+            <Link to={{pathname: `${data}/write`}}>
+                <div className="writePost">
+                    <span className="write-icon" >
+                        <TiPencil size={18}/>
+                    </span>
+                    <span className="write-text">글쓰기</span>
+                </div>
+            </Link>
+        </div>
+
     )
 }
+
 
 export default Board;
--- src/routes/Login.js
+++ src/routes/Login.js
@@ -2,33 +2,53 @@ import React from 'react';
 import {ImArrowLeft2} from "react-icons/im";
 import {Link} from "react-router-dom";
 import mainIcon from "../img/mainimage.svg";
+import {FcGoogle} from "react-icons/fc";
+import {DiApple} from "react-icons/di";
+import kakaoIcon from "../img/kakao vector.png";
+import naverIcon from "../img/simple-icons_naver.png";
+import "./login.css";
 
 function Login() {
     return (
         <>
             <div className="login-head">
 
-                <Link to="/" style={arrowStyle}><ImArrowLeft2 size="30" color="gray"/></Link>
+                <Link to="/"><span>
+                    <ImArrowLeft2 size="25" color="gray"/>
+                </span>
+                    </Link>
 
-                <img style={logoStyle} src={mainIcon} alt=""/>
+                <img src={mainIcon} alt=""/>
 
-                <span style={textStyle}>로그인</span>
+                <span>로그인</span>
             </div>
             <div className="oauth-login">
                 <div className="kakao">
+
                     <a href="https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=79ef8000df154e40bb0f8efcf460e477&redirect_uri=http://localhost:8080/login/kakao">
-                        <input type="button" style={kakaoBtnStyle} value="카카오로 로그인"/></a>
+                        <button><img src={kakaoIcon} alt=""/>카카오로 로그인</button>
+                    </a>
                 </div>
                 <div className="google">
+
                     <a href="https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email&client_id=509614173781-loe251uu4s8osmsdroq8h4h9e6ipi6k5.apps.googleusercontent.com&response_type=code&redirect_uri=http://localhost:8080/login/google&access_type=offline">
-                        <input type="button" style={googleBtnStyle} value="구글로 로그인"/></a>
+                       <button>
+                           <span><FcGoogle size={20}/></span>
+                           구글로 로그인</button>
+                    </a>
                 </div>
-                <div className="naver">
+                <div className="naver" >
                     <a href="https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=t0ileSQ7AIkXzPHtjLZm&redirect_uri=http://localhost:8080/login/naver">
-                        <input type="button" style={naverBtnStyle} value="네이버로 로그인"/></a>
+                        <button><img src={naverIcon} alt=""/>네이버로 로그인</button>
+                    </a>
                 </div>
                 <div className="apple">
-                    <input type="button" style={appleBtnStyle} value="애플로 로그인"/>
+                    <button>
+                        <span>
+                        <DiApple color="white" size={23}/>
+                        </span>
+                        애플로 로그인</button>
+
                 </div>
                 {/*<div className="github">*/}
                 {/*    <a href="https://github.com/login/oauth/authorize?client_id=abd2e29eec7d7a629d88&scope=id,name,email,avatar_url">*/}
@@ -40,67 +60,6 @@ function Login() {
         </>
     )
 }
-
-const arrowStyle = {
-    margin: "200px 10px 10px 50px",
-    display: "inline",
-};
-const logoStyle = {
-    margin: "100px 10px 10px 30px",
-    display: "inline",
-};
-const textStyle = {
-    margin: "100px 10px 10px 50px",
-    display: "inline",
-    fontSize: "2.5em"
-};
-
-const kakaoBtnStyle = {
-    margin: "100px 50px 10px 60px",
-    width: "250px",
-    height: "35px",
-    borderRadius: "12px",
-    backgroundColor: "#FEE500",
-    border: "none",
-}
-
-const naverBtnStyle = {
-    margin: "10px 50px 10px 60px",
-    width: "250px",
-    height: "35px",
-    borderRadius: "12px",
-    backgroundColor: "#03C75A",
-    border: "none",
-    color: "white",
-}
-
-const googleBtnStyle = {
-    margin: "10px 50px 10px 60px",
-    width: "250px",
-    height: "35px",
-    borderRadius: "12px",
-    backgroundColor: "#FFF",
-    border: "1px solid #E0E0E0",
-}
-
-const appleBtnStyle = {
-    margin: "10px 50px 10px 60px",
-    width: "250px",
-    height: "35px",
-    borderRadius: "12px",
-    backgroundColor: "#000000",
-    color: "white",
-    border: "none",
-}
-
-// const githubBtnStyle = {
-//     margin:"10px 50px 10px 60px",
-//     width: "250px",
-//     height: "35px",
-//     borderRadius: "12px",
-//     backgroundColor: "#FEE500",
-//     border:"none",
-// }
 
 
 export default Login;
